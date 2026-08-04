d9f6517c8b5cae2011b1b0fb218f1fada1572278
abhiram4267 <22mh1a4267@acoe.edu.in>
2026-08-04 10:27:11 +0530

    delete backend

--- backend/schemas/chat_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-
-class MessageCreate(BaseModel):
-    senderId: str
-    receiverId: str
-    text: str
-    timestamp: datetime = Field(default_factory=datetime.utcnow)
--- backend/schemas/image_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class ImageResponse(BaseModel):
-    transcription: str
-    prediction: str
-    confidence: str
-    created_at: datetime
